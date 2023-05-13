9a6a0ed26ccd3f1cfeb5826f9381497a011ae8df
kkeroo <jasko10.kerec@gmail.com>
2023-05-13 18:03:05 +0200

    API endpoints added.

--- backend/index.js
+++ backend/index.js
@@ -38,7 +38,72 @@ app.post('/register', (req, res) => {
     });
 });
 
+app.post('/groups', (req, res) => {
+    const group = {
+        name: req.body.name,
+        users: req.body.users
+    };
+    const groups = db.collection('groups');
+    groups.add(group).then(doc => {
+        console.log('Group document created with ID:', doc.id);
+        res.status(201).send('Group document created');
+    }).catch(err => {
+        console.error('Error creating group document:', err);
+        res.status(500).send('Error creating group document');
+    });
+});
 
+app.get('/users/:userID/groups', (req, res) => {
+    let uid = req.params.userID;
+    const groupsCollection = db.collection('groups');
+    groupsCollection.where('users', 'array-contains', uid).get().then(querySnapshot => {
+        let groups = [];
+        querySnapshot.forEach(doc => {
+            const group = doc.data();
+            group.id = doc.id;
+            groups.push(group);
+        });
+        res.status(200).json(groups);
+    }).catch(err => {
+        console.error('Error retrieving groups:', err);
+        res.status(500).send('Error retrieving groups');
+    });
+});
+
+app.post('/expenses', (req, res) => {
+    let expense = {
+        uid: req.body.uid,
+        groupId: req.body.groupId,
+        expenseTitle: req.body.expenseTitle,
+        expensePrice: req.body.expensePrice,
+        createdAt: new Date()
+    };
+    const expensesCollection = db.collection('expenses');
+    expensesCollection.add(expense).then(doc => {
+        console.log('Expense document created with ID:', doc.id);
+        res.status(201).send('Expense document created');
+    }).catch(err => {
+        console.error('Error creating expense document:', err);
+        res.status(500).send('Error creating expense document');
+    });
+});
+
+app.get('/groups/:groupID/expenses', (req, res) => {
+    let groupId = req.params.groupID;
+    const expensesCollection = db.collection('expenses');
+    expensesCollection.where('groupId', '==', groupId).get().then(querySnapshot => {
+        let expenses = [];
+        querySnapshot.forEach(doc => {
+            const expense = doc.data();
+            expense.id = doc.id;
+            expenses.push(expense);
+        });
+        res.status(200).json(expenses);
+    }).catch(err => {
+        console.error('Error retrieving expenses:', err);
+        res.status(500).send('Error retrieving expenses');
+    });
+});
 
 // Start the server
 app.listen(port, () => {
